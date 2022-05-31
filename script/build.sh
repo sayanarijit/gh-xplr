234203fa2fb358ef6cd79764c3cf490bf5992ed5
Arijit Basu <sayanarijit@gmail.com>
2022-06-01 01:22:08 +0530

    Optimize binary size

--- script/build.sh
+++ script/build.sh
@@ -6,4 +6,5 @@ TARGET_TRIPLE=${TARGET_TRIPLE:-x86_64-unknown-linux-gnu}
 GOOS_GOARCH=${GOOS_GOARCH:-linux-amd64}
 
 cargo build --release --locked --target "${TARGET_TRIPLE}"
+strip "target/${TARGET_TRIPLE}/release/gh-xplr"
 mv "target/${TARGET_TRIPLE}/release/gh-xplr" "./dist/${GOOS_GOARCH}"
